f626db84871664c85b9c24587344de61e0bc71f7
vglebovich-lmru <173266533+vglebovich-lmru@users.noreply.github.com>
2024-09-03 09:04:00 +0000

    Revert "fix: use `onApplicationBootstrap` to start processing messages"
    This reverts commit b4b890c7557fcc0aec8f6293c5a3976995791d15.

--- src/consumer/kafkaConsumer.ts
+++ src/consumer/kafkaConsumer.ts
@@ -18,7 +18,7 @@
 
 import vm from "vm";
 
-import { Inject, Injectable, OnApplicationBootstrap } from "@nestjs/common";
+import { Inject, Injectable } from "@nestjs/common";
 import {
   CustomTransportStrategy,
   MessageHandler,
@@ -37,10 +37,7 @@ import { IKafkaConsumerSerializedOptions } from "./interfaces";
 import { KafkaConsumerMessageHandler } from "./kafkaConsumerMessageHandler";
 
 @Injectable()
-export class KafkaConsumer
-  extends Server
-  implements CustomTransportStrategy, OnApplicationBootstrap
-{
+export class KafkaConsumer extends Server implements CustomTransportStrategy {
   public readonly transportId = KafkaConsumerTransportId;
 
   private readonly messageHandlersMap: Map<
@@ -57,6 +54,7 @@ export class KafkaConsumer
   ) {
     super();
   }
+
   public async close(): Promise<void> {
     await Promise.all(
       [...this.consumersMap.values()].map((x) => x.disconnect()),
@@ -64,11 +62,7 @@ export class KafkaConsumer
   }
 
   public listen(callback: (error?: unknown) => void): void {
-    this.connectAndSubscribe().then(callback).catch(callback);
-  }
-
-  public async onApplicationBootstrap(): Promise<void> {
-    await this.startProcessingMessages();
+    this.start().then(callback).catch(callback);
   }
 
   private addMessageHandlerToMap(
@@ -87,12 +81,6 @@ export class KafkaConsumer
     }
   }
 
-  private async connectAndSubscribe(): Promise<void> {
-    await Promise.all([...this.consumersMap.values()].map((x) => x.connect()));
-
-    await this.subscribe();
-  }
-
   private getConsumer(connectionName: string): Consumer {
     const consumer = this.consumersMap.get(connectionName);
 
@@ -105,7 +93,7 @@ export class KafkaConsumer
     return consumer;
   }
 
-  private async startProcessingMessages(): Promise<void> {
+  private async run(): Promise<void> {
     for (const [connectionName, consumer] of this.consumersMap) {
       await consumer.run({
         eachMessage: async (rawPayload) => {
@@ -121,6 +109,13 @@ export class KafkaConsumer
         },
       });
     }
+  }
+
+  private async start(): Promise<void> {
+    await Promise.all([...this.consumersMap.values()].map((x) => x.connect()));
+
+    await this.subscribe();
+    await this.run();
   }
 
   private async subscribe(): Promise<void> {
